8454bfb1546c2f37c6eb936f3f1ab8e3c205679c
muguliebe <muguliebe@gmail.com>
2017-05-10 10:49:11 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": "standard",
+  "plugins": [
+    "standard",
+    "promise"
+  ],
+  "env": {
+    "es6": true,
+    "browser": false,
+    "node": true
+  },
+  "rules": {
+    "indent": ["error", 2],
+    "no-undef": "off",
+    "space-before-function-paren": ["error", {
+        "anonymous": "always",
+        "named": "never",
+        "asyncArrow": "ignore"
+    }],
+    "no-unused-vars": [1, { "vars": "local", "args": "after-used", "ignoreRestSiblings": false }]
+  }
+};
