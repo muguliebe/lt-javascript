472346b79aa4841474bfc6f5d53d91ed2cd69043
muguliebe <muguliebe@gmail.com>
2017-05-10 16:27:21 +0900

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,18 @@
 const gulp = require('gulp')
-const sourcemaps = require('gulp-sourcemaps')
-const babel = require('gulp-babel')
-const run = require('gulp-run')
-const nodemon = require('gulp-nodemon')
-const del = require('del')
-const eslint = require('gulp-eslint')
+const webserver = require('gulp-webserver')
 
 const src = './src'
-const dist = './dist'
 
-gulp.task('clean', function () {
-  del(`${dist}/*`)
+// =============================================================================
+// Web
+gulp.task('web', function() {
+    gulp.src(src + '/')
+        .pipe(webserver({
+            port: 3000,
+            livereload: true,
+            livereloadport: 8283,
+            open: true
+        }))
 })
 
-gulp.task('build', function () {
-  let stream = gulp.src(`${src}/**/*.js`)
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(sourcemaps.write('./', { sourceRoot: `.${src}/` }))
-    .pipe(gulp.dest(`${dist}`))
-  return stream
-})
-
-gulp.task('lint', () => {
-  let stream = gulp.src(['src/**/*.js', '!node_modules/**'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-  // .pipe(eslint.failAfterError())
-  return stream
-})
-
-gulp.task('watch', ['lint', 'build'], function () {
-  // gulp.watch(['src/**/*.js', '!node_modules/**'], ['lint'])
-
-  let stream = nodemon({
-    script: `${dist}/app.js`,
-    watch: src,
-    tasks: ['build']
-  })
-
-  return stream
-})
-
-gulp.task('default', ['clean', 'watch'])
+gulp.task('default', ['web'])
