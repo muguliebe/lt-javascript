489c908c9590bce9df0b789e6e31fe0e974743e2
zany <muguliebe@gmail.com>
2022-01-15 01:14:13 +0900

    change env

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  tabWidth: 4,
+  jsxSingleQuote: true,
+  endOfLine: "lf",
+  arrowParens: "avoid",
+  printWidth: 120,
+};
